3b219c09779486168f33c68c4726b589d3c5c7f6
Corentin Herbert <corentin.herbert@ens-lyon.fr>
2024-11-19 09:42:31 +0100

    update use-package configuration

--- src/emacs.d/lisp/init-packages.el
+++ src/emacs.d/lisp/init-packages.el
@@ -14,11 +14,16 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(setq use-package-verbose t)
+(setq use-package-compute-statistics t)
+(setq use-package-enable-imenu-support t)
 
 (use-package try
   :ensure t)
